1fa9fae9400168b8ee0c11ecd8cc72be45220da5
AustinMaZW <auzzyma@gmail.com>
2021-08-12 13:40:36 +0800

    Initial commit

---
+++ src/main/java/com/example/kampung_unite_web/KampungUniteWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.kampung_unite_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KampungUniteWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KampungUniteWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kampung_unite_web/KampungUniteWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kampung_unite_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KampungUniteWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
