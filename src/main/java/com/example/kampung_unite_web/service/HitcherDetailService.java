b5d567e5a8010760b607a8f75fbe9a32d87c15a1
wow7pm <79070290+wow7pm@users.noreply.github.com>
2021-08-19 03:32:18 +0800

    adjust HitcherDetail

--- src/main/java/com/example/kampung_unite_web/service/HitcherDetailService.java
+++ src/main/java/com/example/kampung_unite_web/service/HitcherDetailService.java
@@ -3,5 +3,5 @@ package com.example.kampung_unite_web.service;
 import com.example.kampung_unite_web.model.HitcherDetail;
 
 public interface HitcherDetailService {
-	public boolean createHitcherDetail(HitcherDetail hd);
+	public HitcherDetail createHitcherDetail(HitcherDetail hd);
 }
