4caba45721397b4d88c0f092605b37ab65411e05
wow7pm <79070290+wow7pm@users.noreply.github.com>
2021-08-19 01:51:50 +0800

    repo
    group Plan controller

---
+++ src/main/java/com/example/kampung_unite_web/service/GroupPlanService.java
@@ -0,0 +1,10 @@
+package com.example.kampung_unite_web.service;
+
+import java.util.List;
+
+import com.example.kampung_unite_web.model.GroupPlan;
+
+public interface GroupPlanService {
+
+	public List<GroupPlan> findGroupPlansByListIds(List<Integer> ids);
+}
