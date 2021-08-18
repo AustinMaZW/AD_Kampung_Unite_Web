4b4bfdca11e88b396154a5ebaa997023a3b56b50
Chor Kian Tang <cktang89@gmail.com>
2021-08-19 03:06:13 +0800

    tidying up duplicate files. tested, able to boot up spring. updated directly to master branch

--- src/main/java/com/example/kampung_unite_web/repositories/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.kampung_unite_web.repositories;
-
-import com.example.kampung_unite_web.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-}
