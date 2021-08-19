a6b3becb0f62c4f9b08f94300d0c59b22dbe9275
yazhen <yazhen94@gmail.com>
2021-08-19 22:18:06 +0800

    Grocery list stuff

--- src/main/java/com/example/kampung_unite_web/repo/GroceryListRepository.java
+++ src/main/java/com/example/kampung_unite_web/repo/GroceryListRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.kampung_unite_web.model.GroceryList;
 
-public interface GroceryListRepository extends JpaRepository<GroceryList, Integer> {
+import java.util.List;
 
+public interface GroceryListRepository extends JpaRepository<GroceryList, Integer> {
+    public List<GroceryList> findGroceryListsByUserDetailId(int id);
 }
