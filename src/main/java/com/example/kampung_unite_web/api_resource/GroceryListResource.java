36e0c779c025e9feeebad56b0f27d5bfc9edfea9
wow7pm <79070290+wow7pm@users.noreply.github.com>
2021-08-21 05:10:10 +0800

    Merge_Test_1_Yue

--- src/main/java/com/example/kampung_unite_web/api_resource/GroceryListResource.java
+++ src/main/java/com/example/kampung_unite_web/api_resource/GroceryListResource.java
@@ -1,19 +1,9 @@
 package com.example.kampung_unite_web.api_resource;
 
 import com.example.kampung_unite_web.model.GroceryList;
-import com.example.kampung_unite_web.model.HitchRequest;
-import com.example.kampung_unite_web.model.HitcherDetail;
-import com.example.kampung_unite_web.model.UserDetail;
-import com.example.kampung_unite_web.model.resposeModel.StatusResponseEntity;
-import com.example.kampung_unite_web.repo.GroceryListRepository;
 import com.example.kampung_unite_web.service.GroceryListService;
-import com.example.kampung_unite_web.service.HitchRequestService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.HttpClientErrorException;
-
 import java.util.List;
 
 @RestController
@@ -27,16 +17,19 @@ public class GroceryListResource {
         return groceryListService.findGroceryListsByUserDetailId(userDetailId);
     }
 
-//    @PutMapping
-//    public ResponseEntity<StatusResponseEntity<GroceryList>> addGroceryList(@RequestBody GroceryList groceryList) {
-//        GroceryList savedItem = groceryListRepository.save(groceryList);
-//        StatusResponseEntity<GroceryList> statusResponseEntity = new StatusResponseEntity<GroceryList>(true, "Added new grocery list to your list",savedItem);
-//        return new ResponseEntity(statusResponseEntity, HttpStatus.CREATED);
-//    }
+    // @PutMapping
+    // public ResponseEntity<StatusResponseEntity<GroceryList>>
+    // addGroceryList(@RequestBody GroceryList groceryList) {
+    // GroceryList savedItem = groceryListRepository.save(groceryList);
+    // StatusResponseEntity<GroceryList> statusResponseEntity = new
+    // StatusResponseEntity<GroceryList>(true, "Added new grocery list to your
+    // list",savedItem);
+    // return new ResponseEntity(statusResponseEntity, HttpStatus.CREATED);
+    // }
 
     @PostMapping("new")
     public GroceryList createGroceryList(@RequestBody GroceryList groceryList) {
-        if(groceryList != null) {
+        if (groceryList != null) {
             groceryListService.createGroceryList(groceryList);
         } else {
             System.out.println("grocery list is null");
@@ -44,4 +37,3 @@ public class GroceryListResource {
         return groceryList;
     }
 }
-
--- src/main/java/com/example/kampung_unite_web/api_resource/UserDetailResource.java
+++ src/main/java/com/example/kampung_unite_web/api_resource/UserDetailResource.java
@@ -2,7 +2,6 @@ package com.example.kampung_unite_web.api_resource;
 
 import com.example.kampung_unite_web.Interfaces.UserDetailService;
 import com.example.kampung_unite_web.model.UserDetail;
-import com.example.kampung_unite_web.model.UserLogin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,59 +17,57 @@ public class UserDetailResource {
     UserDetailService udService;
 
     @GetMapping("find/all")
-    public List<UserDetail> getListUserDetail(){
+    public List<UserDetail> getListUserDetail() {
         List<UserDetail> userDetailList = udService.findAllUsers();
         return userDetailList;
     }
 
     @RequestMapping("find/{id}")
-    public UserDetail findUserById(@PathVariable("id") int id){
+    public UserDetail findUserById(@PathVariable("id") int id) {
         UserDetail ud = udService.findUserById(id);
         System.out.println(ud);
         return ud;
     }
 
-
-    //create put on hold for now
+    // create put on hold for now
     @PostMapping("create")
-    public UserDetail createUserDetail(@RequestBody UserDetail userDetail){
-        if(userDetail != null){
+    public UserDetail createUserDetail(@RequestBody UserDetail userDetail) {
+        if (userDetail != null) {
             udService.createUser(userDetail);
-        }
-        else
+        } else
             System.out.println("userDetail = null");
         return userDetail;
     }
 
-    //update put on hold for now
+    // update put on hold for now
     @PutMapping("update")
-    public void updateUserDetail(@RequestBody UserDetail userDetail){
-        if (userDetail!= null) {
+    public void updateUserDetail(@RequestBody UserDetail userDetail) {
+        if (userDetail != null) {
             udService.updateUser(userDetail);
         }
     }
 
     @DeleteMapping("delete/{id}")
-    public void deleteUserDetail(@PathVariable("id") int id){
-//        System.out.println(udService.findUserById(id)); //test out code only
+    public void deleteUserDetail(@PathVariable("id") int id) {
+        // System.out.println(udService.findUserById(id)); //test out code only
         udService.deleteUserById(id);
     }
 
-//    Uses json in requestbody to pass
+    // Uses json in requestbody to pass
     @PostMapping("login")
-    public UserDetail login (@RequestBody UserDetail userDetail){
+    public UserDetail login(@RequestBody UserDetail userDetail) {
         String authentication = null;
         int userId = 0;
         String udUsername = userDetail.getUsername();
-        UserDetail repoUD= udService.findUserByUsername(udUsername);
-        if (userDetail.getAuthentication()==null || !userDetail.getAuthentication().matches(repoUD.getAuthentication())){
-            if(userDetail.getPassword().matches(repoUD.getPassword())){
+        UserDetail repoUD = udService.findUserByUsername(udUsername);
+        if (userDetail.getAuthentication() == null
+                || !userDetail.getAuthentication().matches(repoUD.getAuthentication())) {
+            if (userDetail.getPassword().matches(repoUD.getPassword())) {
                 userId = repoUD.getId();
                 userDetail.setId(userId);
-                if (repoUD.getAuthentication() != null){
+                if (repoUD.getAuthentication() != null) {
                     authentication = repoUD.getAuthentication();
-                }
-                else{
+                } else {
                     authentication = PasswordEncoder().encode(userDetail.getPassword());
                     repoUD.setAuthentication(authentication);
                     udService.updateUser(repoUD);
@@ -81,65 +78,67 @@ public class UserDetailResource {
         return userDetail;
     }
 
-//    Use url to login (cons: password cannot contain slashes)
-//    @GetMapping("login/{username}&&{password}") // returns an Authentication String
-//    public String login(@PathVariable("username") String username, @PathVariable("password") String password){
-//        String authentication = null;
-//        UserDetail userDetail = udService.findUserByUsername(username); //find user
-//
-//        System.out.println(userDetail.getPassword().toString()); //test purpose only
-//
-//        if (password.matches(userDetail.getPassword().toString())){
-//            if (userDetail.getAuthentication() != null) {
-//                authentication = userDetail.getAuthentication();
-//            }
-//            else {
-//                authentication = PasswordEncoder().encode(password);
-//                userDetail.setAuthentication(authentication);
-//                udService.updateUser(userDetail);
-//            }
-//        }
-//        System.out.println(authentication);//test purpose only
-//        return authentication;
-//    }
+    // Use url to login (cons: password cannot contain slashes)
+    // @GetMapping("login/{username}&&{password}") // returns an Authentication
+    // String
+    // public String login(@PathVariable("username") String username,
+    // @PathVariable("password") String password){
+    // String authentication = null;
+    // UserDetail userDetail = udService.findUserByUsername(username); //find user
+    //
+    // System.out.println(userDetail.getPassword().toString()); //test purpose only
+    //
+    // if (password.matches(userDetail.getPassword().toString())){
+    // if (userDetail.getAuthentication() != null) {
+    // authentication = userDetail.getAuthentication();
+    // }
+    // else {
+    // authentication = PasswordEncoder().encode(password);
+    // userDetail.setAuthentication(authentication);
+    // udService.updateUser(userDetail);
+    // }
+    // }
+    // System.out.println(authentication);//test purpose only
+    // return authentication;
+    // }
 
     private PasswordEncoder PasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @GetMapping("logout/_un={username}")
-    public String logout(@PathVariable("username") String username){
+    public String logout(@PathVariable("username") String username) {
         UserDetail userDetail = udService.findUserByUsername(username);
-        if (userDetail.getAuthentication()!=null){
+        if (userDetail.getAuthentication() != null) {
             udService.logoutUser(userDetail);
         }
         return "logout success";
     }
 
     @GetMapping("logout/_id={id}")
-    public String logout(@PathVariable("id") int id){
+    public String logout(@PathVariable("id") int id) {
         UserDetail userDetail = udService.findUserById(id);
-        if (userDetail.getAuthentication()!=null){
+        if (userDetail.getAuthentication() != null) {
             udService.logoutUser(userDetail);
         }
         return "logout success";
     }
 
     @GetMapping("authenticate/{username}")
-    public String authenticateLogin(@PathVariable("username") String username){
+    public String authenticateLogin(@PathVariable("username") String username) {
         String auth = null;
         UserDetail ud = udService.findUserByUsername(username);
-        if (ud.getAuthentication() != null){
+        if (ud.getAuthentication() != null) {
             auth = ud.getAuthentication();
         }
         return auth;
     }
 
     @PostMapping("authenticate")
-    public UserDetail authenticateLogin(@RequestBody UserDetail userDetail){
+    public UserDetail authenticateLogin(@RequestBody UserDetail userDetail) {
         String auth = null;
         UserDetail ud = udService.findUserByUsername(userDetail.getUsername());
-        if (ud.getAuthentication() != null){
+        if (ud.getAuthentication() != null) {
             auth = ud.getAuthentication();
         }
         return userDetail;
